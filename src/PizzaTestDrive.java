917c7cad24b5546fec991470db97cb3a39a7d0b4
asseke001 <asylzhan12@mail.ru>
2021-10-19 23:00:26 +0600

    Initial commit

---
+++ src/PizzaTestDrive.java
@@ -0,0 +1,13 @@
+public class PizzaTestDrive {
+
+    public static void main(String[] args) {
+        PizzaFactory factory = new PizzaFactory();
+        PizzaStore store = new PizzaStore(factory);
+
+        Pizza pizza = store.orderPizza("cheese");
+        System.out.println("We ordered a " + pizza.getName() + "\n");
+
+        pizza = store.orderPizza("veggie");
+        System.out.println("We ordered a " + pizza.getName() + "\n");
+    }
+}
